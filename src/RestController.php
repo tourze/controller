ad1944c073aa2cf45c68ee99f0f38c3b0242e168
ywisax <25803471@qq.com>
2015-09-15 10:58:34 +0800

    增强RestController功能

--- src/RestController.php
+++ src/RestController.php
@@ -2,6 +2,9 @@
 
 namespace tourze\Controller;
 
+use tourze\Base\Base;
+use tourze\Base\Component\Http;
+
 /**
  * REST控制器
  *
@@ -11,28 +14,65 @@ abstract class RestController extends WebController
 {
 
     /**
-     * GET请求
+     * @var array HTTP方法映射到控制器方法
+     */
+    public $methodMapping = [
+        Http::GET    => 'Get',
+        Http::POST   => 'Post',
+        Http::PUT    => 'Put',
+        Http::DELETE => 'Delete',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepareActionList()
+    {
+        $actions = parent::prepareActionList();
+
+        Base::getLog()->debug(__METHOD__ . ' handle rest controller request', [
+            'method' => $this->request->method,
+        ]);
+
+        if (isset($this->methodMapping[$this->request->method]))
+        {
+            $action = $this->methodMapping[$this->request->method];
+            Base::getLog()->debug(__METHOD__ . ' found rest action', [
+                'method' => $this->request->method,
+                'action' => $action,
+            ]);
+            $actions[] = $action;
+        }
+
+        Base::getLog()->debug(__METHOD__ . ' generate final rest action list', [
+            'actions' => $actions,
+        ]);
+        return $actions;
+    }
+
+    /**
+     * GET请求，获取请求
      *
      * @return mixed
      */
     abstract public function actionGet();
 
     /**
-     * POST请求
+     * POST请求，更新请求
      *
      * @return mixed
      */
     abstract public function actionPost();
 
     /**
-     * PUT请求
+     * PUT请求，创建请求
      *
      * @return mixed
      */
     abstract public function actionPut();
 
     /**
-     * DELETE请求
+     * DELETE请求，删除请求
      *
      * @return mixed
      */
